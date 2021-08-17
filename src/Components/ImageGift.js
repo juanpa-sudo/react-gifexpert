687ea6b38e32f732018ade3483839c15025224a7
Juan Pablo <jpcortesmacias@gmail.com>
2021-08-17 10:44:22 -0500

    eliminando los console.log

--- src/Components/ImageGift.js
+++ src/Components/ImageGift.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function ImageGift({ title, url, id }) {
-  console.log({ title, url, id });
   return (
     <div className="card animate__animated animate__shakeY">
       <h4 className="title_image">{title}</h4>
